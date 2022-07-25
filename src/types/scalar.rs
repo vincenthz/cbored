34b552dae498fb3c82acf9c206aeaca85b2c8cdf
Vincent Hanquez <vincent@typed.io>
2022-07-25 09:35:57 +0100

    add misc-cleanup and documentation for Positive, and add method for Negative

--- src/types/scalar.rs
+++ src/types/scalar.rs
@@ -22,28 +22,53 @@ pub enum Constant {
 }
 
 impl Positive {
+    /// Extract the positive CBOR value into an unsigned value
     pub fn to_u64(&self) -> u64 {
         self.0.to_u64()
     }
 
+    /// Create a canonical Positive element from a u64,
+    /// taking the smallest possible CBOR representation
     pub fn canonical(v: u64) -> Self {
-        match v {
-            _ if v < 24 => Self(Value::Imm(v as u8)),
-            _ if v < 0x100 => Self(Value::U8(v as u8)),
-            _ if v < 0x10000 => Self(Value::U16(v as u16)),
-            _ if v < 0x100000000 => Self(Value::U32(v as u32)),
-            _ => Self(Value::U64(v)),
-        }
+        Self(Value::canonical(v))
     }
 
+    /// Check if the encoded Positive CBOR element have
+    /// the smallest representation possible (canonical)
     pub fn is_canonical(&self) -> bool {
-        match self.0 {
-            // don't check if imm is < 24, as it shouldn't be allowed
-            Value::Imm(_) => true,
-            Value::U8(v) => v >= 24,
-            Value::U16(v) => v >= 0x100,
-            Value::U32(v) => v >= 0x10000,
-            Value::U64(v) => v >= 0x100000000,
-        }
+        self.0.is_canonical()
+    }
+}
+
+impl Negative {
+    /// Extract the negative CBOR value into an unsigned value, which represent
+    /// the integer : -1 - value
+    pub fn negative_u64(&self) -> u64 {
+        self.0.to_u64()
+    }
+
+    /// Try to convert a negative CBOR number into a i64 representing the value
+    ///
+    /// Note this operation might fail as the CBOR representation can represent
+    /// any negative number between -1 and -(2^64-1), whereas i64 represent
+    /// a number between -(2^63) to 2^63-1
+    pub fn to_i64(&self) -> Option<i64> {
+        // use `checked_sub_unsigned` when out of nightly
+        // [https://github.com/rust-lang/rust/issues/87840]
+        i64::try_from(self.0.to_u64())
+            .ok()
+            .and_then(|v| (-1i64).checked_sub(v))
+    }
+
+    /// Create a canonical Positive element from a u64,
+    /// taking the smallest possible CBOR representation
+    pub fn canonical(v: u64) -> Self {
+        Self(Value::canonical(v))
+    }
+
+    /// Check if the encoded Positive CBOR element have
+    /// the smallest representation possible (canonical)
+    pub fn is_canonical(&self) -> bool {
+        self.0.is_canonical()
     }
 }
